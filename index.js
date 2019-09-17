4c072bf383abfe4a0733a7c062ed302a3ea24d4c
Anais Ahmed <anaismahmed@uchicago.edu>
2019-09-17 16:47:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+  let dataobj = {};
+  dataobj.name = name;
+  dataobj.email = email;
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(dataobj)
+  }
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    let header = document.createElement('h1');
+    document.body.appendChild(header);
+    header.append(`${object.id}`);
+
+    console.log(object.id);
+    console.log(header);
+  })
+  .catch(function(error) {
+    alert("o no");
+    document.body.append(error.message);
+  });
+};
